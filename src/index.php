adc37053f0ae4db506e852b3d79002d018c6e3e2
Mirko De Grandis <degami@gmail.com>
2018-11-23 17:25:36 +0100

    commit

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$value = $_SERVER['SERVER_ADDR'];
+
+$db = new PDO('mysql:host=database;dbname=mydb;charset=utf8mb4', 'myuser', 'secret');
+
+$databaseTest = ($db->query('SELECT * FROM dockerSample'))->fetchAll(PDO::FETCH_OBJ);
+
+?>
+
+<html>
+    <body>
+        <h1>Hello, <?= $value ?>!</h1>
+
+        <?php foreach ($databaseTest as $row) : ?>
+            <p>Hello, <?= $row->name ?></p>
+        <?php endforeach; ?>
+
+    </body>
+</html>
